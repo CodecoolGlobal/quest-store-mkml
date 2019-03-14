7310416265840b279a466373477353d9734b9965
Liz <pachaffy@gmail.com>
2019-03-14 13:20:17 +0100

    Implement js for mentor-card-editable.html

---
+++ Scripts/user-cards.js
@@ -0,0 +1,7 @@
+function sendInfoMentorCard() {
+    let mentorName = document.getElementById("mentor-default-name").innerHTML;
+    let mentorEmail = document.getElementById("mentor-default-email").innerHTML;
+    let mentorClass = document.getElementById("mentor-default-class").innerHTML;
+
+    //Here add function that send these data to DB
+}
