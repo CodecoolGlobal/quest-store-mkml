136ce5f3fd7c552d64f2aa05a60fdf9d32b69fc1
plebanm <pleban.marcin@gmail.com>
2019-03-14 13:47:26 +0100

    Create admin->configuration with slider

--- Scripts/script.js
+++ Scripts/script.js
@@ -28,3 +28,103 @@ function sendInfoQuestCard() {
 function changeCardColor(){
     document.getElementById("p2").style.color = "blue";
 }
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdownMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+
+
+// Close the dropdown menu if the user clicks outside of it
+    window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+}
+function slider(){
+    var range = document.getElementById('range');
+
+    noUiSlider.create(range, {
+
+        range: {
+            'min': 0,
+            'max': 5000
+        },
+
+        step: 20,
+
+        // Handles start at ...
+        start: [1000, 2000, 3000, 4000],
+
+        // ... must be at least 300 apart
+        margin: 200,
+
+        // ... but no more than 600
+        limit: 3000,
+
+        // Display colored bars between handles
+        connect: [false, true, true, true, true],
+
+        // Put '0' at the bottom of the slider
+        orientation: 'horizontal',
+
+        // Move handle on tap, bars are draggable
+        behaviour: 'tap-drag',
+        tooltips: true,
+        format: wNumb({
+            decimals: 0
+        }),
+
+        // Show a scale with the slider
+        pips: {
+            mode: 'values',
+            values: [100, 200, 1000, 2000, 3000, 4080],
+            stepped: true,
+            density: 3
+        }
+    });
+
+    // Give the slider dimensions
+    range.style.height = '400px';
+    range.style.margin = '0 auto 30px';
+
+    var valuesDivs = [
+        document.getElementById('range-value-1'),
+        document.getElementById('range-value-2'),
+        document.getElementById('range-value-3'),
+        document.getElementById('range-value-4')
+    ];
+
+    var diffDivs = [
+        document.getElementById('range-diff-1'),
+        document.getElementById('range-diff-2'),
+        document.getElementById('range-diff-3')
+    ];
+
+// When the slider value changes, update the input and span
+    range.noUiSlider.on('update', function (values, handle) {
+        valuesDivs[handle].innerHTML = values[handle];
+        diffDivs[0].innerHTML = values[1] - values[0];
+        diffDivs[1].innerHTML = values[2] - values[1];
+        diffDivs[2].innerHTML = values[3] - values[2];
+    });
+
+    var connect = range.querySelectorAll('.noUi-connect');
+    var classes = ['c-1-color', 'c-2-color', 'c-3-color', 'c-4-color', 'c-5-color'];
+
+    for (var i = 0; i < connect.length; i++) {
+        connect[i].classList.add(classes[i]);
+    }
+
+
+}
+
