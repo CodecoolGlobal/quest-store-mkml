0ff9ce911e673b136cc0cbf803514fa37768b355
Kamil Bednarczyk <kamil2.bednarczyk@gmail.com>
2019-04-10 19:00:38 +0200

    Implement getting all students in mentors classes

--- src/main/java/com/queststore/DAO/UserDAOSql.java
+++ src/main/java/com/queststore/DAO/UserDAOSql.java
@@ -1,8 +1,14 @@
 package com.queststore.DAO;
 
+import com.queststore.Model.Class;
 import com.queststore.Model.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class UserDAOSql implements UserDAO {
@@ -13,6 +19,10 @@ public class UserDAOSql implements UserDAO {
             Optional<User> user = dao.getUser("kamil@bed", "asdfsdf");
             if (user.isPresent()) {
                 System.out.println(user.get().getFirstName());
+                System.out.println(user.get().getUserClass().getName());
+            }
+            for (User u :  dao.getStudentsFrom(1)) {
+                System.out.println(u.getFirstName());
             }
         } catch (DaoException e) {
             e.printStackTrace();
@@ -23,7 +33,8 @@ public class UserDAOSql implements UserDAO {
     public Optional<User> getUser(String email, String password) throws DaoException{
         try (Connection connection = DBCPDataSource.getConnection()){
             PreparedStatement statement = connection.prepareStatement(
-                    "SELECT users.id as id, firstname, lastname, email, classes.name AS class_name, avatar, user_type.name AS type " +
+                    "SELECT users.id as id, firstname, lastname, email, classes.name AS class_name, " +
+                            "avatar, user_type.name AS type, classes.id as classId " +
                             "FROM users " +
                             "JOIN user_type ON users.user_type_id = user_type.id " +
                             "JOIN classes ON users.class_id = classes.id " +
@@ -42,13 +53,38 @@ public class UserDAOSql implements UserDAO {
         }
     }
 
+    public List<User> getStudentsFrom(int classId) throws DaoException {
+        try (Connection connection = DBCPDataSource.getConnection()){
+            List<User> students = new ArrayList<>();
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT users.id as id, firstname, lastname, email, classes.name AS class_name, " +
+                            "avatar, user_type.name AS type, classes.id as classId " +
+                            "FROM users " +
+                            "JOIN user_type ON users.user_type_id = user_type.id " +
+                            "JOIN classes ON users.class_id = classes.id " +
+                            "WHERE classes.id = ? AND user_type.name = 'student' AND users.is_active = true");
+            statement.setInt(1, classId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                students.add(createUser(resultSet));
+            }
+            return students;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DaoException("An error occured during getting students from class from db");
+        }
+    }
+
+
+
     private User createUser(ResultSet resultSet) throws SQLException {
+        Class cls = new Class(resultSet.getInt("classId"), resultSet.getString("class_name"));
         return new User.UserBuilder()
                 .id(resultSet.getInt("id"))
                 .firstName(resultSet.getString("firstname"))
                 .lastName(resultSet.getString("lastname"))
                 .email(resultSet.getString("email"))
-                .classId(resultSet.getString("class_name"))
+                .userClass(cls)
 //                .avatar(resultSet.getBlob("avatar"))
                 .userType(resultSet.getString("type"))
                 .createUser();
--- src/main/java/com/queststore/Services/UserService.java
+++ src/main/java/com/queststore/Services/UserService.java
@@ -1,4 +1,41 @@
 package com.queststore.Services;
 
+import com.queststore.DAO.*;
+import com.queststore.Model.Class;
+import com.queststore.Model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
+
+    private UserDAO userDAO;
+    private ClassDAO classDAO;
+
+    public UserService(UserDAO userDAO, ClassDAO classDAO) {
+        this.userDAO = userDAO;
+        this.classDAO = classDAO;
+    }
+
+    public static void main(String[] args) {
+        UserService userService = new UserService(new UserDAOSql(), new ClassDAOSql());
+        try {
+            for (User u : userService.getAllStudentsInMentorClass(1)) {
+                System.out.println(u.getFirstName());
+            }
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    List<User> getAllStudentsInMentorClass(int mentorId) throws DaoException {
+        List<User> students = new ArrayList<>();
+        List<Class> cls = classDAO.getAllClasses(mentorId);
+        for (Class c : cls) {
+            students.addAll(userDAO.getStudentsFrom(c.getId()));
+        }
+        return students;
+    }
+
 }
