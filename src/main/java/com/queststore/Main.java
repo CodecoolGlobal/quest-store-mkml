cdce00e9f5f16a55f6f3263506af215cef4448ca
Kamil Bednarczyk <kamil2.bednarczyk@gmail.com>
2019-04-10 13:04:45 +0200

    Add Test Main

--- src/main/java/com/queststore/Main.java
+++ src/main/java/com/queststore/Main.java
@@ -1,7 +1,17 @@
 package com.queststore;
 
+import com.queststore.DAO.DBCPDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Connection connection = DBCPDataSource.getConnection();
+            System.out.println("Connection get");
+        } catch (SQLException e) {
+            System.out.println("Error");
+        }
     }
 }
