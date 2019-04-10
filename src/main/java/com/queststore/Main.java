714f7fe2686a016299b104e1691368fbf4eb41ec
plebanm <pleban.marcin@gmail.com>
2019-04-10 11:59:08 +0200

    Create function that create all tables

--- src/main/java/com/queststore/Main.java
+++ src/main/java/com/queststore/Main.java
@@ -1,7 +1,18 @@
 package com.queststore;
 
+import com.queststore.DAO.CreateTables;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+
+        CreateTables createTables = new CreateTables();
+        try {
+            createTables.createAllTables();
+        }catch (SQLException ex){
+            ex.printStackTrace();
+        }
 
     }
 }
