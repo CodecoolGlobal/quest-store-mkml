fd1f0f18159d0cc95ed2038789e6e71a9aa98ad3
plebanm <pleban.marcin@gmail.com>
2019-04-11 23:43:57 +0200

    Create method to get actual coin balance by user id

--- src/main/java/com/queststore/Model/TransactionStatus.java
+++ src/main/java/com/queststore/Model/TransactionStatus.java
@@ -9,4 +9,20 @@ public class TransactionStatus {
         this.id = id;
         this.name = name;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/queststore/Services/UserService.java
+++ src/main/java/com/queststore/Services/UserService.java
@@ -1,12 +1,9 @@
 package com.queststore.Services;
 
 import com.queststore.DAO.*;
+import com.queststore.Model.*;
 import com.queststore.Model.Class;
-import com.queststore.Model.User;
-import com.queststore.Model.UserType;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,21 +11,25 @@ public class UserService {
 
     private UserDAO userDAO;
     private ClassDAO classDAO;
-
-    public UserService(UserDAO userDAO, ClassDAO classDAO) {
+    private TransactionDAO transactionDAO;
+    public UserService(UserDAO userDAO, ClassDAO classDAO, TransactionDAO transactionDAO) {
         this.userDAO = userDAO;
         this.classDAO = classDAO;
+        this.transactionDAO = transactionDAO;
     }
 
-    public static void main(String[] args) {
-        UserService userService = new UserService(new UserDAOSql(), new ClassDAOSql());
-        try {
-            for (User u : userService.getAllStudentsInMentorClass(1)) {
-                System.out.println(u.getFirstName());
-            }
-        } catch (DaoException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws DaoException{
+        UserService userService = new UserService(new UserDAOSql(), new ClassDAOSql(), new TransactionDAOSql());
+
+//        try {
+//            for (User u : userService.getAllStudentsInMentorClass(1)) {
+//                System.out.println(u.getFirstName());
+//            }
+//        } catch (DaoException e) {
+//            e.printStackTrace();
+//        }
+
+        System.out.println(userService.getCoinBalance(1));
 
     }
 
@@ -42,6 +43,30 @@ public class UserService {
     }
 
 
+    public int getCoinBalance(int userId) throws DaoException {
+        List<Transaction> questsList = new ArrayList<>();
+        List<Transaction> artifactsList = new ArrayList<>();
+        int questId = 1;
+        int artifactId = 2;
+
+        questsList.addAll(transactionDAO.getTransactions(userId, questId));
+        artifactsList.addAll(transactionDAO.getTransactions(userId, artifactId));
+
+        Integer coinBalance = 0;
+        for (Transaction transaction : questsList) {
+            if (transaction.getTransactionStatus().getName().equals("accepted")) {
+                coinBalance += transaction.getCost();
+            }
+        }
+
+        for (Transaction transaction : artifactsList) {
+            if (transaction.getTransactionStatus().getName().equals("accepted")) {
+                coinBalance -= transaction.getCost();
+            }
+        }
+
+        return coinBalance;
+    }
 
 
 }
